d2685b09e11144ff94a6f7004502f7e712e0023e
Lipen <lipen00@gmail.com>
2017-03-03 22:04:29 +0300

    Fix parser

--- lipen/read_bed.py
+++ lipen/read_bed.py
@@ -1,12 +1,13 @@
 import csv
 from collections import namedtuple
 
-Item = namedtuple('Peak', ['chr', 'start', 'end', ])
+Item = namedtuple('Item', ['chr', 'start', 'end', 'feature', 'score', 'strand'])
 
 filename = '../data/dm6_rRNA.bed'
 
 with open(filename) as f:
     reader = csv.reader(f, delimiter='\t')
-    data = [Item(item) for item in reader]
+    data = [Item(*item) for item in reader]
 
 print('LEN DATA: {}'.format(len(data)))
+assert print(data[0].chr) == 'chr4'
